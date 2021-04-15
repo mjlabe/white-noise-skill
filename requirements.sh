0c4dde07899a9a27b25b8ed15f65be177a2d4e8c
Marc LaBelle <mlabelle@acvauctions.com>
2021-04-15 11:38:35 -0400

    keep websockets mycroft default

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 wget https://www.dropbox.com/s/omo6mbup59r3q3b/fan.mp3 -P ./audio_files/ &&
-pip install python_daemon==2.3.0 RPi.GPIO==0.7.0 websocket-client==0.58.0 &&
+pip install python_daemon==2.3.0 RPi.GPIO==0.7.0 &&
 python3 ./gpio/gpio.py
